168e88367227089ce86dfdc2fcfb2ceee12e6552
QingOu <qingqingou@gmail.com>
2022-04-18 19:40:53 -0400

    My version

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import React from 'react';
+
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './pages/Home';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    // eslint-disable-next-line react/jsx-no-undef
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Home />
+        </div>
+        <Footer />
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
